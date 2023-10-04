87c532519a3c907ba145cba28ca99a59d0607ea0
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2023-10-04 15:18:51 +0300

    added dockerized postgresql db and connected to it using TypeORM and added the coffee entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      // database = DB name
+      autoLoadEntities: true,
+      // helps load the modules automatically instead of specifying the entities array
+      synchronize: true,
+      // To sync TypeORM entities with the database every time we run our app
+      // lets typeORM automatically generate the SQL table from all classes with the @Entity() decorator and the metadata they contain.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
